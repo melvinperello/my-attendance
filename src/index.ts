42e7fd62ba88a8212e9b70915b104f140287c77f
Melvin Perello <melvinperello@gmail.com>
2022-10-29 01:22:00 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import fastify from "fastify";
 import ejs from "ejs";
 import path from "path";
+import QRCode from "qrcode";
+import { authenticator } from "otplib";
 
 const { PLATFORM } = process.env;
 const app = fastify({
@@ -28,8 +30,34 @@ app.get("/", async (request, reply: any) => {
   return reply.view("/templates/index.ejs", { text: "Google is Fun!" });
 });
 
-app.post("/login", async (request: any, reply: any) => {
+app.get("/register", async (request, reply: any) => {
+  const otpauth = authenticator.keyuri(
+    "melvin",
+    "my-attendance",
+    "AQRHIXRGFVYHUGQC"
+  );
+  const qrCode = await QRCode.toDataURL(otpauth, {
+    errorCorrectionLevel: "H",
+    width: 200,
+    scale: 10,
+    margin: 2,
+  });
+  return reply.view("/templates/register.ejs", {
+    otpImage: qrCode,
+    otpString: "AQRHIXRGFVYHUGQC",
+  });
+});
+
+app.post("/api/login", async (request: any, reply: any) => {
   return reply.send({ message: request.body });
+});
+
+app.post("/api/register", async (request: any, reply: any) => {
+  const { code } = request.body;
+  // const secret = authenticator.generateSecret(); // base32 encoded hex secret key // AQRHIXRGFVYHUGQC
+
+  const ok = authenticator.check(code, "AQRHIXRGFVYHUGQC");
+  return reply.send({ message: ok });
 });
 
 //---------------------------------------------
