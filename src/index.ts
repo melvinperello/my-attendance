169cb8982d62c519b22f75f3fe0ae0abdfa35f42
Melvin Perello <melvinperello@gmail.com>
2022-10-28 17:08:32 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import "reflect-metadata";
 import fastify from "fastify";
 import ejs from "ejs";
+import path from "path";
 
 const app = fastify({
-  logger: true, // you can also define the level passing an object configuration to logger: {level: 'debug'}
+  logger: false, // you can also define the level passing an object configuration to logger: {level: 'debug'}
 });
+
 app.register(require("@fastify/view"), {
   engine: {
     ejs,
   },
 });
+app.register(require("@fastify/static"), {
+  root: path.join(process.cwd(), "public"),
+  prefix: "/public/", // optional: default '/'
+});
+
 app.addContentTypeParser("application/json", {}, (req, body: any, done) => {
   done(null, body.body);
 });
+
 app.get("/", async (request, reply: any) => {
   return reply.view("/templates/index.ejs", { text: "Google is Fun!" });
 });
