22f022627c02db5352a01c2ba1baf7391bb8d4d3
Melvin Perello <melvinperello@gmail.com>
2022-10-29 05:04:25 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import "reflect-metadata";
 import fastify from "fastify";
 import ejs from "ejs";
 import path from "path";
 import QRCode from "qrcode";
 import { authenticator } from "otplib";
-
+const {Firestore} = require('@google-cloud/firestore');
+const firestore = new Firestore();
 const { PLATFORM } = process.env;
+
 const app = fastify({
   logger: false, // you can also define the level passing an object configuration to logger: {level: 'debug'}
 });
@@ -31,33 +32,76 @@ app.get("/", async (request, reply: any) => {
 });
 
 app.get("/register", async (request, reply: any) => {
-  const otpauth = authenticator.keyuri(
-    "melvin",
-    "my-attendance",
-    "AQRHIXRGFVYHUGQC"
-  );
-  const qrCode = await QRCode.toDataURL(otpauth, {
-    errorCorrectionLevel: "H",
-    width: 200,
-    scale: 10,
-    margin: 2,
-  });
-  return reply.view("/templates/register.ejs", {
-    otpImage: qrCode,
-    otpString: "AQRHIXRGFVYHUGQC",
-  });
+  const document = firestore.doc('melvin/auth');
+
+  const authDoc = await document.get();
+  
+
+  if(authDoc.exists){
+    const {verified} = authDoc.data();
+    // user not verified.
+    if(!verified){
+      // create new secret.
+      const secret = authenticator.generateSecret(); // base32 encoded hex secret key
+      // update the secret in database.
+      await document.update({
+        secret: secret,
+      });
+      // create QR Image.
+      const otpauth = authenticator.keyuri(
+        "melvin",
+        "my-attendance",
+        secret
+      );
+      // create image string.
+      const qrCode = await QRCode.toDataURL(otpauth, {
+        errorCorrectionLevel: "H",
+        width: 200,
+        scale: 10,
+        margin: 2,
+      });
+      // display site.
+      return reply.view("/templates/register.ejs", {
+        otpImage: qrCode,
+        otpString: secret,
+      });
+    }
+  }
 });
 
 app.post("/api/login", async (request: any, reply: any) => {
-  return reply.send({ message: request.body });
+  const {username,code} = request.body;
+  const document = firestore.doc(`${username}/auth`);
+  const authDoc = await document.get();
+  if(authDoc.exists){
+    const {secret} = authDoc.data();
+    const ok = authenticator.check(code, secret);
+    if(ok){
+      return reply.send({ message: "ok" });
+    }else{
+      return reply.send({ message: "not_ok" });
+    }
+  }else{
+    return reply.send({ message: "not_found" });
+  }
 });
 
 app.post("/api/register", async (request: any, reply: any) => {
   const { code } = request.body;
   // const secret = authenticator.generateSecret(); // base32 encoded hex secret key // AQRHIXRGFVYHUGQC
-
-  const ok = authenticator.check(code, "AQRHIXRGFVYHUGQC");
-  return reply.send({ message: ok });
+  const document = firestore.doc('melvin/auth');
+  const authDoc = await document.get();
+  if(authDoc.exists){
+    const {secret} = authDoc.data();
+    const ok = authenticator.check(code, secret);
+    if(ok){
+      await document.update({
+        verified: true,
+      });
+      return reply.send({ message: "ok" });
+    }
+  }
+  return reply.send({ message: "not_ok" });
 });
 
 //---------------------------------------------
