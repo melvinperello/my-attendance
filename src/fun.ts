402254637e7daf9494627c6f8d9d4617ba343405
Melvin Perello <melvinperello@gmail.com>
2022-10-31 00:24:30 +0800

    update tz

--- src/fun.ts
+++ src/fun.ts
@@ -1,252 +1,246 @@
 import { authenticator } from "otplib";
 import QRCode from "qrcode";
-import moment from "moment";
-const {Firestore} = require('@google-cloud/firestore');
+import moment from "moment-timezone";
+const { Firestore } = require("@google-cloud/firestore");
 const firestore = new Firestore();
 
-const getDoc = async (name:string)=>{
-    const document = firestore.doc(name);
-    const doc = await document.get();
-    if(doc.exists){
-        return doc;
-    }else{
-        return null;
-    }
-}
-
-
-const getAuthDoc = async (username:string) =>{
-    const doc = await getDoc(`authorization/${username}`)
+const getDoc = async (name: string) => {
+  const document = firestore.doc(name);
+  const doc = await document.get();
+  if (doc.exists) {
     return doc;
-}
+  } else {
+    return null;
+  }
+};
 
-const updateAuthDoc = async (username:string,data:any) =>{
-    const document = firestore.doc(`authorization/${username}`);
-    await document.update(data);
-}
+const getMoment = () => {
+  return moment().tz("Asia/Taipei");
+};
 
+const getAuthDoc = async (username: string) => {
+  const doc = await getDoc(`authorization/${username}`);
+  return doc;
+};
 
+const updateAuthDoc = async (username: string, data: any) => {
+  const document = firestore.doc(`authorization/${username}`);
+  await document.update(data);
+};
 
-export const logAttendance = async (username:string,group:string,status:string)=>{
-    const current = moment().format("YYYY-MM-DD")
-    const timein = moment().format("HH:mm");
-    const docName = `attendance/${current}/${group}/${username}`;
+export const logAttendance = async (
+  username: string,
+  group: string,
+  status: string
+) => {
+  const current = getMoment().format("YYYY-MM-DD");
+  const timein = getMoment().format("HH:mm");
+  const docName = `attendance/${current}/${group}/${username}`;
 
-
-    const doc = await getDoc(docName);
-    if(doc){
-        return {
-            code: 400,
-            message: "already_logged",
-            data: doc.data()
-        }
-    }
-    const document = firestore.doc(docName);
-    const data = await document.set({
-        status: status,
-        timein: timein
-    });
+  const doc = await getDoc(docName);
+  if (doc) {
     return {
-        code: 201,
-        message: "created",
-        data: data
-    }
-}
+      code: 400,
+      message: "already_logged",
+      data: doc.data(),
+    };
+  }
+  const document = firestore.doc(docName);
+  const data = await document.set({
+    status: status,
+    timein: timein,
+  });
+  return {
+    code: 201,
+    message: "created",
+    data: data,
+  };
+};
 
-
-export const checkAttendance = async (username:string,group:string)=>{
-    const current = moment().format("YYYY-MM-DD")
-    const docName = `attendance/${current}/${group}/${username}`;
-    const doc = await getDoc(docName);
-    if(doc){
-        return {
-            code: 200,
-            message: "already_logged",
-            data: doc.data()
-        }
-    }else{
-        return {
-            code: 404,
-            message: "no_attendance",
-            data: null
-        }
-    }
-}
-
-export const groupie = async (group:string)=>{
-    const current = moment().format("YYYY-MM-DD")
-    const docName = `attendance/${current}/${group}`;
-    const collectionReference = firestore.collection(docName);
-    const attendances = await collectionReference
-    .orderBy('timein')
-    .get();
-    const attendancesData = attendances.docs.map((d:any) => {
-        const thisDocData = d.data();
-        let label = ""
-        if(thisDocData.status === "PRESENT"){
-            label="success"
-        } else  if(thisDocData.status === "SICK_LEAVE"){
-            label="dark"
-        }else  if(thisDocData.status === "EMERGENCY_LEAVE"){
-            label="danger"
-        }
-        return {
-            ...thisDocData,
-            name:d.id,
-            label: label
-        }
-    });
+export const checkAttendance = async (username: string, group: string) => {
+  const current = getMoment().format("YYYY-MM-DD");
+  const docName = `attendance/${current}/${group}/${username}`;
+  const doc = await getDoc(docName);
+  if (doc) {
     return {
-        code: 200,
-        message: "ok",
-        data: attendancesData
-    }
-}
+      code: 200,
+      message: "already_logged",
+      data: doc.data(),
+    };
+  } else {
+    return {
+      code: 404,
+      message: "no_attendance",
+      data: null,
+    };
+  }
+};
 
-
-
-export const login = async (username:string,code:string)=>{
-    const doc = await getAuthDoc(username);
-    if(!doc){
-        return {
-            code: 404,
-            message: "user_does_not_exists",
-            data: null
-        }
-    }
-    const {secret,verified} = doc.data();
-    if(!verified){
-        return {
-            code: 401,
-            message: "user_not_verified",
-            data: null
-        }
-    }
-    const validated = authenticator.check(code, secret);
-    if(!validated){
-        return {
-            code: 401,
-            message: "wrong_code",
-            data: null
-        }
+export const groupie = async (group: string) => {
+  const current = getMoment().format("YYYY-MM-DD");
+  const docName = `attendance/${current}/${group}`;
+  const collectionReference = firestore.collection(docName);
+  const attendances = await collectionReference.orderBy("timein").get();
+  const attendancesData = attendances.docs.map((d: any) => {
+    const thisDocData = d.data();
+    let label = "";
+    if (thisDocData.status === "PRESENT") {
+      label = "success";
+    } else if (thisDocData.status === "SICK_LEAVE") {
+      label = "dark";
+    } else if (thisDocData.status === "EMERGENCY_LEAVE") {
+      label = "danger";
     }
     return {
-        code: 200,
-        message: "ok",
-        data: null
-    }
-}
+      ...thisDocData,
+      name: d.id,
+      label: label,
+    };
+  });
+  return {
+    code: 200,
+    message: "ok",
+    data: attendancesData,
+  };
+};
 
-export const register = async (username:string,code:string)=>{
-    const doc = await getAuthDoc(username);
-    if(!doc){
-        return {
-            code: 404,
-            message: "user_does_not_exists",
-            data: null
-        }
-    }
-    const {secret,verified} = doc.data();
-    if(verified){
-        return {
-            code: 400,
-            message: "user_already_associated",
-            data: null
-        }
-    }
-    const validated = authenticator.check(code, secret);
-    if(!validated){
-        return {
-            code: 401,
-            message: "wrong_code",
-            data: null
-        }
-    }
-    // --------------------------------------
-    // Verfied Updated
-    // --------------------------------------
-    await updateAuthDoc(username,{
-        verified: true,
-    })
-    // --------------------------------------
+export const login = async (username: string, code: string) => {
+  const doc = await getAuthDoc(username);
+  if (!doc) {
     return {
-        code: 200,
-        message: "ok",
-        data: {
-            username
-        }
-    }
-}
-
-export const check = async (username:string)=>{
-    const doc = await getAuthDoc(username);
-    if(!doc){
-        return {
-            code: 404,
-            message: "user_does_not_exists",
-            data: null
-        }
-    }
-    const {verified} = doc.data();
-    if(verified){
-        return {
-            code: 400,
-            message: "user_already_associated",
-            data: {
-                username
-            }
-        }
-    }
+      code: 404,
+      message: "user_does_not_exists",
+      data: null,
+    };
+  }
+  const { secret, verified } = doc.data();
+  if (!verified) {
     return {
-        code: 200,
-        message: "ok",
-        data: {
-            username
-        }
-    }
-}
+      code: 401,
+      message: "user_not_verified",
+      data: null,
+    };
+  }
+  const validated = authenticator.check(code, secret);
+  if (!validated) {
+    return {
+      code: 401,
+      message: "wrong_code",
+      data: null,
+    };
+  }
+  return {
+    code: 200,
+    message: "ok",
+    data: null,
+  };
+};
 
+export const register = async (username: string, code: string) => {
+  const doc = await getAuthDoc(username);
+  if (!doc) {
+    return {
+      code: 404,
+      message: "user_does_not_exists",
+      data: null,
+    };
+  }
+  const { secret, verified } = doc.data();
+  if (verified) {
+    return {
+      code: 400,
+      message: "user_already_associated",
+      data: null,
+    };
+  }
+  const validated = authenticator.check(code, secret);
+  if (!validated) {
+    return {
+      code: 401,
+      message: "wrong_code",
+      data: null,
+    };
+  }
+  // --------------------------------------
+  // Verfied Updated
+  // --------------------------------------
+  await updateAuthDoc(username, {
+    verified: true,
+  });
+  // --------------------------------------
+  return {
+    code: 200,
+    message: "ok",
+    data: {
+      username,
+    },
+  };
+};
 
-export const preRegister = async (username:string)=>{
-    const doc = await getAuthDoc(username);
-    if(!doc){
-        return {
-            code: 404,
-            message: "user_does_not_exists",
-            data: null
-        }
-    }
-    const {verified} = doc.data();
-    if(verified){
-        return {
-            code: 400,
-            message: "user_already_associated",
-            data: null
-        }
-    }
-    //
-    const secret = authenticator.generateSecret(); // base32 encoded hex secret key
-    await updateAuthDoc(username,{
-        secret: secret,
-        verified: false
-    })
-    const otpauth = authenticator.keyuri(
+export const check = async (username: string) => {
+  const doc = await getAuthDoc(username);
+  if (!doc) {
+    return {
+      code: 404,
+      message: "user_does_not_exists",
+      data: null,
+    };
+  }
+  const { verified } = doc.data();
+  if (verified) {
+    return {
+      code: 400,
+      message: "user_already_associated",
+      data: {
         username,
-        "ur-attendance",
-        secret
-    );
-    const qrCode = await QRCode.toDataURL(otpauth, {
-        errorCorrectionLevel: "H",
-        width: 200,
-        scale: 10,
-        margin: 2,
-    });
+      },
+    };
+  }
+  return {
+    code: 200,
+    message: "ok",
+    data: {
+      username,
+    },
+  };
+};
+
+export const preRegister = async (username: string) => {
+  const doc = await getAuthDoc(username);
+  if (!doc) {
     return {
-        code: 200,
-        message: "ok",
-        data: {
-            secretString: secret,
-            secretImageString: qrCode
-        }
-    }
-}
+      code: 404,
+      message: "user_does_not_exists",
+      data: null,
+    };
+  }
+  const { verified } = doc.data();
+  if (verified) {
+    return {
+      code: 400,
+      message: "user_already_associated",
+      data: null,
+    };
+  }
+  //
+  const secret = authenticator.generateSecret(); // base32 encoded hex secret key
+  await updateAuthDoc(username, {
+    secret: secret,
+    verified: false,
+  });
+  const otpauth = authenticator.keyuri(username, "ur-attendance", secret);
+  const qrCode = await QRCode.toDataURL(otpauth, {
+    errorCorrectionLevel: "H",
+    width: 200,
+    scale: 10,
+    margin: 2,
+  });
+  return {
+    code: 200,
+    message: "ok",
+    data: {
+      secretString: secret,
+      secretImageString: qrCode,
+    },
+  };
+};
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import "dotenv/config";
 import fastify from "fastify";
 import ejs from "ejs";
 import path from "path";
-import { login, check, preRegister, register,logAttendance,checkAttendance,groupie } from "./fun";
+import {
+  login,
+  check,
+  preRegister,
+  register,
+  logAttendance,
+  checkAttendance,
+  groupie,
+} from "./fun";
 const { PLATFORM, JWT_PRIVATE_KEY, JWT_PUBLIC_KEY, JWT_KEY_PHRASE } =
   process.env;
 
@@ -29,10 +37,8 @@ app.register(require("@fastify/view"), {
 app.register(require("@fastify/static"), {
   root: path.join(process.cwd(), "public"),
   prefix: "/public/", // optional: default '/',
-  maxAge: PLATFORM ? 300000: 0,
+  maxAge: PLATFORM ? 300000 : 0,
 });
-
-
 
 if (PLATFORM === "gcp") {
   app.addContentTypeParser("application/json", {}, (req, body: any, done) => {
@@ -40,163 +46,157 @@ if (PLATFORM === "gcp") {
   });
 }
 
-app.get('/robots.txt', function (req, reply:any) {
-  reply.code(200).header("Cache-Control","max-age:300").header("Content-Type","text/plain; charset=utf-8").send(`User-agent: *\nDisallow: /`)
-})
+app.addHook("onSend", async function (request, reply) {
+  reply.headers({
+    "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+    "Content-Security-Policy":
+      "default-src 'self' cdn.jsdelivr.net code.jquery.com",
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Options": "DENY",
+    "Referrer-Policy": "no-referrer",
+    "Permissions-Policy": "geolocation=(), microphone=()",
+    "X-XSS-Protection": "1; mode=block",
+  });
+});
 
-app.register(
-  function (isolated, opts, done) {
-    isolated.addHook("onSend", async function (request, reply) {
-      reply.headers({
-        "Cache-Control": "no-store",
-        "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-        "Content-Security-Policy":"default-src 'self' cdn.jsdelivr.net code.jquery.com",
-        "X-Content-Type-Options": "nosniff",
-        "X-Frame-Options": "DENY",
-        "Referrer-Policy": "no-referrer",
-        "Permissions-Policy":"geolocation=(), microphone=()",
-        "X-XSS-Protection":"1; mode=block"
-      });
+app.register(function (isolated, opts, done) {
+  isolated.addHook("onSend", async function (request, reply) {
+    reply.headers({
+      "Cache-Control": "no-store",
     });
+  });
 
-    isolated.get("/", async (request, reply: any) => {
-      return reply.view("/templates/index.ejs");
-    });
-    
-    isolated.get("/login/:username", async (request: any, reply: any) => {
-      const { username } = request.params;
-      const data = await check(username);
-      if (data.message !== "ok" && data.message !== "user_already_associated") {
-        reply.code(data.code).send(data);
-      }
-      return reply.view("/templates/login.ejs", { username: username });
-    });
-    
-    isolated.get("/pre-register/:username", async (request: any, reply: any) => {
-      const { username } = request.params;
-      const data = await preRegister(username);
-      if (data.code !== 200) {
-        return reply.code(data.code).send(data);
-      }
-      return reply.code(data.code).view("/templates/pre-register.ejs", {
-        ...data.data,
-        username: username,
-      });
-    });
-    
-    // ---------------------------------------------------------------------------------------
-    // API's
-    // ---------------------------------------------------------------------------------------
-    isolated.post("/api/check", async (request: any, reply: any) => {
-      const { username } = request.body;
-      const data = await check(username);
+  isolated.get("/", async (request, reply: any) => {
+    return reply.view("/templates/index.ejs");
+  });
+
+  isolated.get("/login/:username", async (request: any, reply: any) => {
+    const { username } = request.params;
+    const data = await check(username);
+    if (data.message !== "ok" && data.message !== "user_already_associated") {
       reply.code(data.code).send(data);
-    });
-    
-    isolated.post("/api/login", async (request: any, reply: any) => {
-      const { code, username } = request.body;
-      const data = await login(username, code);
-      if (data.code === 200 && data.message === "ok") {
-        const token = await reply.jwtSign({ username,group:"core" });
-    
-        reply.code(data.code).send({
-          ...data,
-          data: {
-            token,
-          },
-        });
-      } else {
-        reply.code(data.code).send(data);
-      }
-    });
-    
-    isolated.post("/api/register", async (request: any, reply: any) => {
-      const { code, username } = request.body;
-      const data = await register(username, code);
-      reply.code(data.code).send(data);
-    });
+    }
+    return reply.view("/templates/login.ejs", { username: username });
+  });
 
-    // ---------------------------------------------------------------------------------------
-// PROTECTED
-// ---------------------------------------------------------------------------------------
-app.register(
-  function (main, opts, done) {
-    main.addHook("onRequest", async (request: any, reply) => {
-      const qsToken = request.query.token;
-      try {
-        if (qsToken) {
-          
-          try{
-            
-            // @ts-expect-error: jwt is decorated fastify expect unknown error
-            await app.jwt.verify(qsToken);
-          }catch(ex:any){
-            if(ex.code === "FAST_JWT_EXPIRED"){
-            // @ts-expect-error: jwt is decorated fastify expect unknown error
-            const {username} = app.jwt.decode(qsToken);
-              reply.redirect("/login/"+username+"?message=expired")
-            }else{
-              throw ex;
-            }
-          }
-          
-        } else {
-          await request.jwtVerify();
-        }
-      } catch (err) {
-        reply.send(err);
-      }
-    });
-
-    main.get("/", async (request:any, reply: any) => {
-      // @ts-expect-error: jwt is decorated fastify expect unknown error
-      const {username,group} = app.jwt.decode(request.query.token);
-      const data = await checkAttendance(username,group);
-      if(data.code === 200){
-        // get all attendance
-        const myGroupie = await groupie(group);
-        console.log(myGroupie)
-        return reply.view("/templates/main.ejs",{
-          code: data.code,
-          status: data.data.status,
-          timein: data.data.timein,
-          username: username,
-          group:group,
-          tableData: myGroupie.data
-        });
-      }else{
-        return reply.view("/templates/main.ejs",{
-          code: data.code,
-          status: "",
-          timein: "",
-          username:username,
-          group:group,
-          tableData: []
-        });
-      }
-    });
-
-    main.post("/api/attendance", async (request:any, reply: any) => {
-      const { selected } = request.body;
-      const {username,group} = request.user;
-      const data = await logAttendance(username,group,selected);
+  isolated.get("/pre-register/:username", async (request: any, reply: any) => {
+    const { username } = request.params;
+    const data = await preRegister(username);
+    if (data.code !== 200) {
       return reply.code(data.code).send(data);
+    }
+    return reply.code(data.code).view("/templates/pre-register.ejs", {
+      ...data.data,
+      username: username,
     });
+  });
 
-    done();
-  },
-  {
-    prefix: "/main",
-  }
-);
+  // ---------------------------------------------------------------------------------------
+  // API's
+  // ---------------------------------------------------------------------------------------
+  isolated.post("/api/check", async (request: any, reply: any) => {
+    const { username } = request.body;
+    const data = await check(username);
+    reply.code(data.code).send(data);
+  });
 
-    done();
-  })
+  isolated.post("/api/login", async (request: any, reply: any) => {
+    const { code, username } = request.body;
+    const data = await login(username, code);
+    if (data.code === 200 && data.message === "ok") {
+      const token = await reply.jwtSign({ username, group: "core" });
+
+      reply.code(data.code).send({
+        ...data,
+        data: {
+          token,
+        },
+      });
+    } else {
+      reply.code(data.code).send(data);
+    }
+  });
+
+  isolated.post("/api/register", async (request: any, reply: any) => {
+    const { code, username } = request.body;
+    const data = await register(username, code);
+    reply.code(data.code).send(data);
+  });
+
+  // ---------------------------------------------------------------------------------------
+  // PROTECTED
+  // ---------------------------------------------------------------------------------------
+  app.register(
+    function (main, opts, done) {
+      main.addHook("onRequest", async (request: any, reply) => {
+        const qsToken = request.query.token;
+        try {
+          if (qsToken) {
+            try {
+              // @ts-expect-error: jwt is decorated fastify expect unknown error
+              await app.jwt.verify(qsToken);
+            } catch (ex: any) {
+              if (ex.code === "FAST_JWT_EXPIRED") {
+                // @ts-expect-error: jwt is decorated fastify expect unknown error
+                const { username } = app.jwt.decode(qsToken);
+                reply.redirect("/login/" + username + "?message=expired");
+              } else {
+                throw ex;
+              }
+            }
+          } else {
+            await request.jwtVerify();
+          }
+        } catch (err) {
+          reply.send(err);
+        }
+      });
+
+      main.get("/", async (request: any, reply: any) => {
+        // @ts-expect-error: jwt is decorated fastify expect unknown error
+        const { username, group } = app.jwt.decode(request.query.token);
+        const data = await checkAttendance(username, group);
+        if (data.code === 200) {
+          // get all attendance
+          const myGroupie = await groupie(group);
+          console.log(myGroupie);
+          return reply.view("/templates/main.ejs", {
+            code: data.code,
+            status: data.data.status,
+            timein: data.data.timein,
+            username: username,
+            group: group,
+            tableData: myGroupie.data,
+          });
+        } else {
+          return reply.view("/templates/main.ejs", {
+            code: data.code,
+            status: "",
+            timein: "",
+            username: username,
+            group: group,
+            tableData: [],
+          });
+        }
+      });
+
+      main.post("/api/attendance", async (request: any, reply: any) => {
+        const { selected } = request.body;
+        const { username, group } = request.user;
+        const data = await logAttendance(username, group, selected);
+        return reply.code(data.code).send(data);
+      });
+
+      done();
+    },
+    {
+      prefix: "/main",
+    }
+  );
+
+  done();
+});
 //---------------------------------------------
-
-
-
-
 
 //---------------------------------------------
 export const fastifyFunction = async (request: any, reply: any) => {
